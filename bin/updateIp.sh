a8061aa3e74455d100e04b550bf7d4975f07d78d
Dave McEwan <dave.mcewan@bristol.ac.uk>
2021-05-31 12:04:47 +0100

    updateIp Make more robust against SSH dropouts and complex format of .ssh/config.

--- bin/updateIp.sh
+++ bin/updateIp.sh
@@ -1,9 +1,20 @@
 
+# Update a remote record of this machine's IP address.
+#
 # Use with a crontab something like this:
-# * * * * * /home/dm16886/ucfg/bin/updateIp.sh
+# * * * * * /home/dm16886/ucfg/bin/updateIp.sh snowy d
 
-# Update SSH config to correct IP address.
-IPADDR="`ip addr | grep enp0s31f6 | grep -Po '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(?=/)'`"
-ssh snowy 'cat .ssh/config' | head -n-1 > .snowy_ssh_config
-echo "    Hostname ${IPADDR}" >> .snowy_ssh_config
-scp .snowy_ssh_config snowy:.ssh/config
+REMOTE_HOSTNAME="$1"
+
+if [ "$2" = "" ]; then
+  LOCAL_HOSTNAME="`hostname`"
+else
+  LOCAL_HOSTNAME="$2"
+fi
+LOCAL_DATE="`date`"
+LOCAL_IPADDR="`ip addr | grep enp0s31f6 | grep -Po '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(?=/)'`"
+
+echo "${LOCAL_DATE} ${LOCAL_IPADDR}" | ssh ${REMOTE_HOSTNAME} "cat >> ${LOCAL_HOSTNAME}.ipAddr"
+
+#REMOTE_SCRIPT="ucfg/bin/updateSshHostname.sh"
+#ssh ${REMOTE_HOSTNAME} "${REMOTE_SCRIPT} ${LOCAL_HOSTNAME} ${LOCAL_IPADDR}"
